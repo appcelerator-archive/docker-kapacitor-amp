24d39b92a16f8aef8ea4fc94599d0ad4df954b7a
Nicolas Degory <ndegory@axway.com>
2016-10-28 13:22:20 +0200

    debug docker hub

--- sut/test.sh
+++ sut/test.sh
@@ -57,12 +57,21 @@ while [[ $nb -eq 0 ]]; do
   sleep 1
   r=$(curl $KAPACITOR_HOST:9092/kapacitor/v1/debug/vars 2>/dev/null | jq '.kapacitor | map(select(.name == "ingress") + select(.tags.database == "telegraf") + select(.tags.retention_policy == "default"))')
   nb=$(echo $r | jq 'length')
-  if [[ $i -gt 35 ]]; then break; fi
+  if [[ $i -gt 50 ]]; then break; fi
   echo -n "+"
 done
 if [[ $nb -lt 1 ]]; then
   echo
   echo "failed ($nb subscriptions)"
+  curl $KAPACITOR_HOST:9092/kapacitor/v1/debug/vars 2>/dev/null
+  echo "Running containers:"
+  docker ps
+  ci=$(docker ps -a | grep /influxdb | head -1 | awk '{print $1}')
+  echo "logs from influxdb $ci:"
+  docker logs $ci
+  ck=$(docker ps -a | grep /kapacitor | head -1 | awk '{print $1}')
+  echo "logs from kapacitor $ck:"
+  docker logs $ck
   exit 1
 fi
 echo "($nb) [OK]"
@@ -80,8 +89,10 @@ done
 if [[ $nb -lt 1 ]]; then
   echo
   echo "failed ($nb subscriptions)"
+  curl $KAPACITOR_HOST:9092/kapacitor/v1/debug/vars 2>/dev/null
   exit 1
 fi
 echo "($nb points) [OK]"
+curl $KAPACITOR_HOST:9092/kapacitor/v1/debug/vars 2>/dev/null | jq '.kapacitor | map(select(.name == "ingress") + select(.tags.database == "telegraf") + select(.tags.retention_policy == "default"))'
 
 echo "all tests passed successfully"
